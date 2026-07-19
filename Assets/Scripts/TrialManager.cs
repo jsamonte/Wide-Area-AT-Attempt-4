41d849c617941c2bf908454845389b2d3186867a
Jared V. Samonte <jsamonte@lamar.edu>
2026-07-19 18:51:39 -0500

    Everything worked.

--- Assets/Scripts/TrialManager.cs
+++ Assets/Scripts/TrialManager.cs
@@ -33,6 +33,9 @@ public class TrialManager : MonoBehaviour
 
     private void Start()
     {
+        // Cap the framerate to 60fps to drastically reduce heat generation on Magic Leap
+        Application.targetFrameRate = 60;
+
         // Safety checks to ensure other scripts don't start automatically
         if (spawner != null) spawner.spawnOnAwake = false;
         if (tracker != null) tracker.recordOnAwake = false;
